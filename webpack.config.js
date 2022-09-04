470d654b131dce96ce9ced9fe0ae672022092d40
devrahul <rbhadoria97@gmail.com>
2022-09-04 15:44:45 +0530

    change in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 const { VueLoaderPlugin } = require("vue-loader");
 const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
+  mode: "production",
   entry: {
     index: "./src/main.js",
   },
@@ -26,6 +27,11 @@ module.exports = {
       assert: require.resolve("assert/"),
       stream: false,
     },
+  },
+  output: {
+    filename: "[name].js",
+    publicPath: "/scripts/",
+    path: path.resolve(__dirname, "../public/scripts"),
   },
   optimization: {
     minimizer: [
