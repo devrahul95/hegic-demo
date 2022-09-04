875e4391c13e221279ab70678a8098f3ea8c4a87
devrahul <rbhadoria97@gmail.com>
2022-09-04 16:57:38 +0530

    add

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const webpack = require("webpack");
 const { VueLoaderPlugin } = require("vue-loader");
 const TerserPlugin = require("terser-webpack-plugin");
 module.exports = {
-  mode: "production",
   entry: {
     index: "./src/main.js",
   },
@@ -28,11 +27,6 @@ module.exports = {
       stream: false,
     },
   },
-  output: {
-    filename: "[name].js",
-    publicPath: "/scripts/",
-    path: path.resolve(__dirname, "../public/scripts"),
-  },
   optimization: {
     minimizer: [
       new TerserPlugin({
@@ -43,6 +37,9 @@ module.exports = {
         },
       }),
     ],
+  },
+  devServer: {
+    allowedHosts: "all",
   },
   plugins: [new VueLoaderPlugin()],
 };
